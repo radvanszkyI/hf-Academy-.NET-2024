5dc881003b4c05a51a49ed1d972bf3951ce4688c
radvanszkyI <45069028+radvanszkyI@users.noreply.github.com>
2024-03-05 07:59:00 +0100

    Add project files.

---
+++ Academy-2024/Models/Course.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Academy_2024.Models
+{
+    public class Course
+    {
+        [Required]
+        public int? Id { get; set; }
+
+        [StringLength(20)]
+        public string? Name { get; set; }
+
+        public string? Description { get; set; }
+    }
+}
---
+++ Academy-2024/Repositories/CourseRepository.cs
@@ -0,0 +1,50 @@
+﻿using Academy_2024.Models;
+
+namespace Academy_2024.Repositories
+{
+    public class CourseRepository
+    {
+        private static List<Course> Courses=new List<Course> { new Course { Id = 1, Name = "Math", Description = "Upper matematic" } };
+
+        public List<Course> GetAll() { return Courses; }
+
+        public Course? GetById(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id) { return course; }
+            }
+            return null;
+        }
+
+        public void Create(Course data)
+        {
+            Courses.Add(data);
+        }
+
+        public Course? Update(int id, Course data) {
+            foreach (Course course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    course.Name = data.Name;
+                    course.Description = data.Description;
+                    return course;
+                }
+            }
+            return null;
+        }
+        public bool Delete(int id)
+        {
+            foreach (var course in Courses)
+            {
+                if (course.Id == id)
+                {
+                    Courses.Remove(course);
+                    return true;
+                }
+            }
+            return false;
+        }
+    }
+}
